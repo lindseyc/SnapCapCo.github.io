08ab7034645f1f79adfca54db66509fd1bc61d3f
Anisha Kaul <35541715+hmc-cs-akaul@users.noreply.github.com>
2019-04-18 14:03:29 -0700

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-# models.py potentially to use to get model
-
-class Mood(models.Model):
-    sentiment = models.CharField(max_length=50)
-                    # upload_to specifies which directory the images go to (django automatically creates a media directory)
-    image = models.ImageField(upload_to='images/')
