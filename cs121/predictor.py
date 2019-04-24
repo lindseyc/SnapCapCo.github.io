49f58fd835febac4da634f26793e5b7567a7db04
Lindsey Cleary <lindseyc@users.noreply.github.com>
2019-04-23 22:37:08 -0700

    update predictor.py
    remove unnecessary conditions, just returning the index from mode_predict

--- cs121/predictor.py
+++ cs121/predictor.py
@@ -33,21 +33,6 @@ def model_predict(img_path, model_path):
     learn = load_model(model_path)
     img = open_image(img_path)
     pred_class,pred_idx,outputs = learn.predict(img)
-    if classes[pred_idx] == 'disgusted':
-        randValue = random.randint(0,len(disgusted))
-        caption = 'disgusted! ' + ' Your Caption is: ' +  disgusted[randValue][2] + ' From: ' + disgusted[randValue][0] + ' by: '  + disgusted[randValue][1]
-        return caption
-    elif classes[pred_idx] == 'angry':
-        randValue = random.randint(0,len(angry))
-        caption = 'angry! ' + ' Your Caption is: ' +  angry[randValue][2] + ' From: ' + angry[randValue][0] + ' by: '  + angry[randValue][1]
-        return caption
-    elif classes[pred_idx] == 'sad':
-        randValue = random.randint(0,len(sad))
-        caption = 'sad! ' + ' Your Caption is: ' +  sad[randValue][2] + ' From: ' + sad[randValue][0] + ' by: '  + sad[randValue][1]
-        return caption
-    else:
-        randValue = random.randint(0,len(happy))
-        caption = 'happy! ' + ' Your Caption is: ' +  happy[randValue][2] + ' From: ' + happy[randValue][0] + ' by: '  + happy[randValue][1] 
-        return pred_idx
+    return pred_idx
 
 
